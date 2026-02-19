5a67b95ad211ad1fba9b04f69ecdbc4fc2f146d5
Praanesh S <praaneshsb@gmail.com>
2026-02-19 12:31:42 +0530

    commit - env updated commit - test added

---
+++ main/test.py
@@ -0,0 +1,11 @@
+from main.circuit import Circuit
+from main.gates import H, RZ
+
+qc = Circuit(int(input("Enter n:")))
+
+qc.apply(H(0))
+qc.apply(RZ(2.4,0))
+
+print("Statevector:", qc.get_statevector())
+print("Probabilities:", qc.measure_probabilities())
+print("Counts:", qc.run(shots=1000))
