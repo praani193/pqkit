3e43991c4fc871b4e14bd39cd5fc505ce0c2a554
Praanesh S <praaneshsb@gmail.com>
2026-02-19 15:09:09 +0530

    commit - test added

--- main/test.py
+++ main/test.py
@@ -4,7 +4,7 @@ from main.gates import H, RZ
 qc = Circuit(int(input("Enter n:")))
 
 qc.apply(H(0))
-qc.apply(RZ(2.4,0))
+qc.apply(RZ(1,0))
 
 print("Statevector:", qc.get_statevector())
 print("Probabilities:", qc.measure_probabilities())
