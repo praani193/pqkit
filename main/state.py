a7441859a38808c98c0db0a64407a012e9c79a4d
Praanesh S <praaneshsb@gmail.com>
2026-02-19 16:37:18 +0530

    commit - state.py - removed U - Unitary

--- main/state.py
+++ main/state.py
@@ -1,16 +1,17 @@
 import numpy as np
 
-
-class QuantumState:
-    def __init__(self, n):
-        self.n = n
-        self.dim = 2**n
+class StateVector:
+    def __init__(self, n_qubits):
+        self.n_qubits = n_qubits
+        self.dim = 2 ** n_qubits
         self.state = np.zeros(self.dim, dtype=complex)
         self.state[0] = 1.0
-    def apply_unitary(self, U):
-        self.state = U @ self.state
-    def probabilities(self):
+
+    def apply_gate(self, gate):
+        gate.apply(self)
+
+    def measure_probabilities(self):
         return np.abs(self.state) ** 2
 
     def __repr__(self):
-        return f"QuantumState(n={self.n}, state={self.state})"
+        return str(self.state)
