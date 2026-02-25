92a200540ce0f5dd82ec4e7b8d85b9769a64134b
Praanesh S <praaneshsb@gmail.com>
2026-02-25 19:24:58 +0530

    commit - test.py - unit test

--- main/test.py
+++ main/test.py
@@ -1,11 +1,17 @@
 from main.gates import H, CNOT
 from main.circuit import QuantumCircuit
 
-qc = QuantumCircuit(5)
+qc = QuantumCircuit(2)
 qc.add_gate(H(0))
 qc.add_gate(CNOT(0, 1))
 
-final_state = qc.run()
+# Single run
+state = qc.run()
+print("Statevector:", state.state)
 
-print(final_state.state)
-print(final_state.measure_probabilities())
+# Single measurement
+print("Measured:", state.measure_all())
+
+# Shot-based execution
+counts = qc.run_shots(1000)
+print("Counts:", counts)
