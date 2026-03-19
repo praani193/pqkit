88851f02aed3b42e4784313e09615ed7bcf03cb1
Praanesh S <praaneshsb@gmail.com>
2026-03-19 14:15:20 +0530

    commit - qgan_test.py - updated

--- examples/qgan_test.py
+++ examples/qgan_test.py
@@ -1,8 +1,28 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 from models.qgan import QGAN
+from datasets.gaussian import sample_real_data
 
+
+# Initialize model
 model = QGAN(
     lr=0.05,
-    epochs=1000000
+    epochs=10000,
+    batch_size=64
 )
 
-model.train()
+# Train
+model.train(sample_real_data)
+
+
+# ===== Compare distributions =====
+real = sample_real_data(1000)
+fake = model.G.sample(1000)
+
+plt.hist(real, bins=50, alpha=0.5, label="Real")
+plt.hist(fake, bins=50, alpha=0.5, label="Fake")
+
+plt.legend()
+plt.title("QGAN Learning Distribution")
+plt.show()
