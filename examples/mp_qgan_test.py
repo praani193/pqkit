df8585f378214f9dc3a961eba44fc768f61c3404
Praanesh S <praaneshsb@gmail.com>
2026-03-24 13:08:29 +0530

    commit - mp_qgan_test.py

--- examples/mp_qgan_test.py
+++ examples/mp_qgan_test.py
@@ -2,28 +2,29 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from models.mp_qgan import MultiQGAN
-from datasets.gaussian import sample_real_data
+from datasets.gaussian_new import sample_real_data
 
 
 model = MultiQGAN(
-    lr=0.05,
-    epochs=3000,
-    batch_size=64
-)
+        epochs=501,
+        batch_size=64
+    )
 
 model.train(sample_real_data)
 
-
+print(model.G1.theta, model.G2.theta)
 # ===== Compare distributions =====
 
+
+""""
 real = sample_real_data(1000)
 fake1 = model.G1.sample(500)
-fake2 = model.G2.sample(500)
 
 import matplotlib.pyplot as plt
 
 plt.hist(real, bins=50, alpha=0.5, label="Real")
-plt.hist(list(fake1)+list(fake2), bins=50, alpha=0.5, label="Fake")
+plt.hist(list(fake1), bins=50, alpha=0.5, label="Fake")
 
 plt.legend()
 plt.show()
+"""
